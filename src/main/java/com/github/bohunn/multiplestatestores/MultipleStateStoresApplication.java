65459fcae09aaff5a34c5da881870a3228e663f2
bohunn <borun.p@hotmail.com>
2023-08-31 09:53:43 +0200

    init commit

---
+++ src/main/java/com/github/bohunn/multiplestatestores/MultipleStateStoresApplication.java
@@ -0,0 +1,13 @@
+package com.github.bohunn.multiplestatestores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleStateStoresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultipleStateStoresApplication.class, args);
+    }
+
+}
